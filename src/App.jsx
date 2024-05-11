b2e2a2bd40d918a9b4c8868a2f7048b8e2123b9c
ny64 <furry@imnyang.xyz>
2024-05-11 21:22:55 +0900

    furry commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,105 +7,48 @@ let PLAYING = "Cloudless";
 function App() {
   const [playing, setPlaying] = useState("Cloudless");
   const [status, setStatus] = useState("pause");
-  
   const [musics, setMusics] = useState([]);
-  const audioContainer = useRef();
-  const audioSource = useRef();
-  const progress = useRef();
-  const volume = useRef();
+  /** @type {React.MutableRefObject<HTMLAudioElement>} */
+  const audioContainer = useRef()
+
+  /** @type {React.MutableRefObject<HTMLSourceElement>} */
+  const audioSource = useRef()
+  
+  /** @type {React.MutableRefObject<HTMLInputElement>} */
+  const progress = useRef()
 
   useEffect(() => {
-    fetchMusicList();
-
-    const interval = setInterval(fetchMusicList, 10000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const fetchMusicList = () => {
-    fetch("https://fback.imnyang.xyz//NY64_Cover/list")
-      .then((res) => res.text())
-      .then((data) => {
-        setMusics(eval(data));
-      });
-  };
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (audioContainer.current) {
-        let overPlay =
-          progress.current.value -
-          (audioContainer.current.currentTime /
-            audioContainer.current.duration) *
-            1000;
-        if (String(overPlay) === "NaN") overPlay = 0;
-        if (
-          document.hasFocus() &&
-          (overPlay > 4 || overPlay < -4) &&
-          !audioContainer.current.paused
-        ) {
-          audioContainer.current.currentTime =
-            (progress.current.value / 1000) * audioContainer.current.duration;
-        } else {
-          let progressPercent =
-            (audioContainer.current.currentTime /
-              audioContainer.current.duration) *
-            1000;
-          if (String(progressPercent) === "NaN") progressPercent = 0;
-          progress.current.value = progressPercent;
-          if (progressPercent === 1000) {
-            musicNext();
-            progress.current.value = 0;
-            audioContainer.current.currentTime = 0;
-          }
-        }
-      }
-    }, 100);
-
-    return () => clearInterval(intervalId);
-  }, [musics]);
+    if (START == 1) return
+    START = 1
+    fetch(`https://fback.imnyang.xyz//NY64_Cover/list?${Date.now()}`).then(res => res.text().then(data => {
+      setMusics(eval(data))
+    }));
+    setInterval(() => {
+      fetch(`https://fback.imnyang.xyz//NY64_Cover/list?${Date.now()}`).then(res => res.text().then(data => {
+        setMusics(eval(data))
+      }));
+    }, 10000)
+    volume.current.value = 100
+    progress.current.value = 0
+    audioContainer.current.currentTime = 0
+  }, [])
 
   useEffect(() => {
-    if (status === "play") {
-      if (
-        audioSource.current.src !==
-        `https://fback.imnyang.xyz//NY64_Cover/Cover/${playing}.mp3`
-      ) {
+    if (status == "play") {
+      if (!decodeURI(audioSource.current.src).startsWith(`https://fback.imnyang.xyz//NY64_Cover/Cover/${playing}.mp3`)) {
+        let date = Date.now();
         navigator.mediaSession.metadata = new MediaMetadata({
           title: playing,
           album: "NY Music",
           artwork: [
-            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg`, sizes: '96x96', type: 'image/png' },
-            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg`, sizes: '128x128', type: 'image/png' },
-            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg`, sizes: '192x192', type: 'image/png' },
-            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg`, sizes: '256x256', type: 'image/png' },
-            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg`, sizes: '384x384', type: 'image/png' },
-            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg`, sizes: '512x512', type: 'image/png' },
-          ] 
+            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg?${date}`, sizes: '96x96', type: 'image/png' },
+            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg?${date}`, sizes: '128x128', type: 'image/png' },
+            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg?${date}`, sizes: '192x192', type: 'image/png' },
+            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg?${date}`, sizes: '256x256', type: 'image/png' },
+            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg?${date}`, sizes: '384x384', type: 'image/png' },
+            { src: `https://fback.imnyang.xyz//NY64_Cover/Image/${playing}.jpg?${date}`, sizes: '512x512', type: 'image/png' },
+          ]
         });
-        navigator.mediaSession.setActionHandler(
-            'nexttrack',
-            () => musicNext()
-        );
-        navigator.mediaSession.setActionHandler(
-            'previoustrack',
-            () => musicPrev()
-        );
-        navigator.mediaSession.setActionHandler(
-            'play',
-            () => setStatus("play")
-        );
-        navigator.mediaSession.setActionHandler(
-            'pause',
-            () => setStatus("pause")
-        );
-        navigator.mediaSession.setActionHandler(
-            'stop',
-            () => {
-                audioContainer.current.pause();
-                audioContainer.current.currentTime = 0;
-            }
-        );
         navigator.mediaSession.setActionHandler(
           'nexttrack',
           musicNext
@@ -118,16 +61,18 @@ function App() {
           progress.current.value = e.seekTime*10
           audioContainer.current.currentTime = e.seekTime
         });
-        progress.current.value = 0;
-        audioSource.current.src = `https://fback.imnyang.xyz//NY64_Cover/Cover/${playing}.mp3`;
-        audioContainer.current.load();
-        
+        audioSource.current.src = `https://fback.imnyang.xyz//NY64_Cover/Cover/${playing}.mp3?${date}`
+        audioContainer.current.load()
+
+        progress.current.value = 0
+        audioContainer.current.currentTime = 0
       }
-      audioContainer.current.play();
+      audioContainer.current.play()
     } else {
-      audioContainer.current.pause();
+      audioContainer.current.pause()
     }
-  }, [playing, status]);
+
+  }, [playing, status])
 
   function musicNext() {
     PLAYING = musics[(musics.indexOf(PLAYING) + 1) % musics.length];
